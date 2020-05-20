b9c71f65fd78d6cc29f2d2e53e20ead42bd63e85
Dongkey-Dev <Dongkey-Dev@gmail.com>
2020-05-21 02:04:17 +0900

    remove useless directory and files

--- AndroidStudioProjects/MyApplication/app/src/NoticeBoard/app/src/main/java/com/example/noticeboard/Adapters/MainData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.noticeboard.Adapters
-
-data class MainData(val photo: String, val Title: String, val Creator: String, val PostDate: String, val ViewCount: Int, val PostContents: String?)
--- AndroidStudioProjects/MyApplication/app/src/TestRecyclerview/app/src/main/java/com/example/testrecyclerview/MainData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.testrecyclerview
-
-data class MainData(val title:String, val content:String)
