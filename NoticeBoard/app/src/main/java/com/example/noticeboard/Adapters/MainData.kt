411d33b79b413902221654b830ee80a4fd567ac6
Dongkey-Dev <Dongkey-Dev@gmail.com>
2020-05-25 20:32:23 +0900

    200525 : fix recyclerview

--- NoticeBoard/app/src/main/java/com/example/noticeboard/Adapters/MainData.kt
+++ NoticeBoard/app/src/main/java/com/example/noticeboard/Adapters/MainData.kt
@@ -1,3 +1,9 @@
 package com.example.noticeboard.Adapters
 
-data class MainData(val photo: String, val Title: String, val Creator: String, val PostDate: String, val ViewCount: Int, val PostContents: String?)
+data class MainData(
+    val photo: String,
+    val Title: String,
+    val Creator: String,
+    val PostDate: String,
+    val ViewCount: Int,
+    val PostContents: String?)
